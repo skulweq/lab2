deb71c5627b5cfc50d22f812b58b549d75d013f9
Эмиль Закирьянов <m2410778@edu.misis.ru>
2025-05-27 22:37:29 +0300

    added test_basic.cpp and library.cpp and CMakeLists.txt

--- path_delivery-skulweq/include/graph.hpp
+++ path_delivery-skulweq/include/graph.hpp
@@ -6,14 +6,19 @@
 #include <queue>
 #include <cstdint>
 
-namespace template_library {
-    /// Sample Graph structure for implementing
+extern int INF;
+
+namespace graph_library {
     class Graph {
-        struct Edge { std::string to; uint16_t weight; };
+    private:
+        struct Edge {
+            std::string to;
+            int weight;
+        };
         std::unordered_map<std::string, std::vector<Edge>> adj;
 
     public:
-        virtual void add_edge(const std::string& from, const std::string& to, uint16_t weight);
+        virtual void add_edge(const std::string& from, const std::string& to, int weight);
         virtual std::vector<std::string> find_shortest_path(const std::string& start, const std::string& end);
     };
 }
--- path_delivery-skulweq/src/library.cpp
+++ path_delivery-skulweq/src/library.cpp
@@ -1,24 +1,51 @@
 #include "graph.hpp"
 #include <limits>
 
-namespace template_library {
+int INF = std::numeric_limits<int>::max();
 
-    void Graph::add_edge(const std::string &from, const std::string &to, uint16_t weight) {
-        adj[from].push_back(Edge{to, weight});
+namespace graph_library {
+    void Graph::add_edge(const std::string& u, const std::string& v, int w) {
+        adj[u].push_back(Edge{ v, w });
+        adj[v].push_back(Edge{ u, w });
     }
-
-    std::vector<std::string> Graph::find_shortest_path([[maybe_unused]] const std::string &start,
-                                                       [[maybe_unused]] const std::string &end) {
+    std::vector<std::string> Graph::find_shortest_path(const std::string& start, const std::string& end) {
         using Pair = std::pair<int, std::string>;
-        std::priority_queue<Pair, std::vector<Pair>, std::greater<>> pq;
-        std::unordered_map<std::string, int> dist;
-        std::unordered_map<std::string, std::string> prev;
-        std::vector<std::string> path;
+        std::priority_queue <Pair, std::vector<Pair>, std::greater<>> pq;
+        std::unordered_map <std::string, int> dist;
+        std::unordered_map <std::string, std::string> prev;
+        std::vector <std::string> path;
 
-        /*
-         * Shortest Path Realization
-         */
-
+        for (auto& node : adj) {
+            dist[node.first] = INF;
+        }
+        dist[start] = 0;
+        pq.push({ 0, start });
+        while (!pq.empty()) {
+            auto& current = pq.top();
+            pq.pop();
+            int d = current.first;
+            std::string s = current.second;
+            if (d > dist[s]) {
+                continue;
+            }
+            for (const Edge& edge : adj[s]) {
+                std::string v = edge.to;
+                int weight = edge.weight;
+                if (dist[v] > dist[s] + weight) {
+                    dist[v] = dist[s] + weight;
+                    prev[v] = s;
+                    pq.push({ dist[v], v });
+                }
+            }
+        }
+        if (dist[end] == INF) {
+            return path;
+        }
+        for (std::string at = end; at != start; at = prev[at]) {
+            path.push_back(at);
+        }
+        path.push_back(start);
+        std::reverse(path.begin(), path.end());
         return path;
     }
 }
